ae163ec3dc5c6bb8c03f31087f33c38fc8643481
unknown <pimenteleduca@gmail.com>
2022-03-10 17:59:46 -0600

    10/03/2022

--- EjemploArchivo/src/com/jpimentel/pg/Main.java
+++ EjemploArchivo/src/com/jpimentel/pg/Main.java
@@ -1,8 +1,6 @@
 package com.jpimentel.pg;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Main {
 
@@ -10,26 +8,41 @@ public class Main {
 	// write your code here
         //Escritura y lectura de un archivo .txt
         //Escritura
-
         //FileWrite - crea un archivo en una ruta indicada y lo apertura
         FileWriter fw;
-
         //PrintWrite - permite agregar informacion a un archivo aperturado
         PrintWriter pw;
-
         try {
             fw = new FileWriter("C:\\Users\\Jonathan Pimentel\\Desktop\\Ejemplo\\Ejemplo.txt");
             pw = new PrintWriter(fw);
 
             pw.println("Esta es una prueba de escritura para el archivo recien creado");
-            for (int i = 0; i <= 30; i++){
+            for (int i = 0; i <= 10; i++){
                 pw.println("Linea "+i);
             }
+            pw.println("Hola como estan");
             fw.close();
         }catch (Exception e){
             System.err.println("Error en el archivo por "+e);
         }
+        //Lectura
+        //File - encontrar un archivo en una ruta indicada
+        File f;
+        //FileReader - valida el archivo encontrado por file y lo apertura
+        FileReader fr;
+        //BufferedReader - lee la informacion completa del archivo hasta su tope vacio
+        BufferedReader br;
+        try {
+            f = new File("C:\\Users\\Jonathan Pimentel\\Desktop\\Ejemplo\\Ejemplo.txt");
+            fr = new FileReader(f);
+            br = new BufferedReader(fr);
 
-
+            String lineas;
+            while ((lineas = br.readLine()) != null){
+                System.out.println(lineas);
+            }
+        }catch (Exception e){
+            System.err.println("Mensaje "+e+" */*");
+        }
     }
 }
